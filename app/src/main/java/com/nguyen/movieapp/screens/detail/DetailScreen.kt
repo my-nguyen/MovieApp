6e20af8f0d743f4d4dcf9c9344cfcd56c931330f
Mỹ Nguyễn <nguyen@MacBook-Pro-M1Max-32G-1T.local>
2025-09-24 19:15:02 -0700

    b8: Navigating back on back button clicked

--- app/src/main/java/com/nguyen/movieapp/screens/detail/DetailScreen.kt
+++ app/src/main/java/com/nguyen/movieapp/screens/detail/DetailScreen.kt
@@ -2,26 +2,38 @@ package com.nguyen.movieapp.screens.detail
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
 @Composable
 fun DetailScreen(navController: NavController, movieId: String?) {
-    Surface(modifier = Modifier
-        .fillMaxHeight()
-        .fillMaxWidth()) {
+    Surface(
+        modifier = Modifier
+            .fillMaxHeight()
+            .fillMaxWidth()
+    ) {
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center
         ) {
             Text("movieId: $movieId", style = MaterialTheme.typography.headlineMedium)
+            Spacer(modifier = Modifier.height(23.dp))
+            Button(onClick = {
+                navController.popBackStack()
+            }) {
+                Text("Go back")
+            }
         }
     }
 }
